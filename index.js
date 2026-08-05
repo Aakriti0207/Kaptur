0d4169fbd3c5b0c940335875e5506f993610e68d
Aakriti Arya <aakritiarya2005@gmail.com>
2026-08-05 23:39:56 +0530

    feat: implement server setup with express, cors, and cookie-parser; add utility classes for API responses and errors

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-import express from "express";
 import { configDotenv } from "dotenv";
 import { connectDB } from "./src/DB/indexDB";
 
 connectDB()
-
-const app = express();
-
+.then(
+    () => {
+        app.on("error", (error) => {
+            console.log("Error-" , error)
+            throw error
+        })
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at ${process.env.PORT}`)
+        })
+    }
+)
+.catch((error) => {
+    console.log("DB Connection Error - ", error);
+})
---
+++ src/utils/apiRes.js
@@ -0,0 +1,12 @@
+class ApiRes {
+    constructor(
+        statusCode,
+        data,
+        message = "Success"
+    ){
+        this.statusCode = statusCode,
+        this.data = data,
+        this.message = message
+    }
+}
+export { ApiRes }
