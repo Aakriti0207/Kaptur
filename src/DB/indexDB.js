9c6ecf543ab36e4c05b1dfcfaa8e4eb3a6c83c4d
Aakriti Arya <aakritiarya2005@gmail.com>
2026-08-06 20:50:35 +0530

    fix: database connection & file structure

--- src/DB/indexDB.js
+++ src/DB/indexDB.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants";
+import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try{
-        const connecInstance = await mongoose.connect('${process.env.MONGODB_URI/{DB-NAME}')
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     }catch(error){
         console.log("MongoDb Connection error - " , error);
         process.exit(1);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import { configDotenv } from "dotenv";
-import { connectDB } from "./src/DB/indexDB";
+import dotenv from "dotenv";
+import { connectDB } from "./DB/indexDB.js";
+import { app } from "./app.js";
+
+dotenv.config({
+    path: './.env'
+})
 
 connectDB()
 .then(
     () => {
-        app.on("error", (error) => {
-            console.log("Error-" , error)
-            throw error
-        })
         app.listen(process.env.PORT || 8000, () => {
             console.log(`Server is running at ${process.env.PORT}`)
         })
